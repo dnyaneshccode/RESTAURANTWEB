9ed3167d23e2b2766deda6f4d352fd9fd57e4dfe
Dnyaneshc <dnyaneshchavan5555@gmail.com>
2021-02-18 13:44:02 +0530

    FIRST-COMMIT

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+       autoplay:true,
+      slidesToShow:3,
+      slidesToScroll:1,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+      responsive:[
+         {
+            breakpoint:992,
+            settings:{
+             slidesToShow:2,
+            }
+         },
+         {
+          breakpoint:768,
+          settings:{
+           slidesToShow:1,
+          }
+       }
+      ]
+ 
+    });
+ 
+    $('.nav-trigger').click(function(){
+       $('.site-content-wrapper').toggleClass('scaled');
+    })
+ });
+
+ var countDownDate = new Date("Sep 25, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+  var date = new Date().toDateString();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+document.getElementById("day").innerHTML = days;
+document.getElementById("hour").innerHTML = hours;
+document.getElementById("min").innerHTML = minutes;
+document.getElementById("sec").innerHTML = seconds;
+document.getElementById("dates").innerHTML = date;
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
